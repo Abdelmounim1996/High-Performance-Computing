72623ec1ba286afaa376030b3e22195cf3483f6d
Abdelmounim1996 <abdelmounim.elkorchi@gmail.com>
2023-10-20 00:15:45 +0200

    update code

--- OpenMP/Ex0.cpp
+++ OpenMP/Ex0.cpp
@@ -9,12 +9,21 @@ int main(int argc, char *argv[])
     // set number of threads
     omp_set_num_threads(6);
     // parallel region begin
+#pragma omp parallel num_threads(3)
+    {
+        int id = omp_get_thread_num();
+        printf("|all threads : %d |Hello World from thread %d\n",
+               omp_get_num_threads(), id);
+    }
+    // parallel region end
+    printf("***********************************************************\n");
+    // parallel region begin
 #pragma omp parallel
     {
         // set thread id
         int id = omp_get_thread_num();
         // print thread id
-        printf("|Hello from thread : %d |\n", id);
+        printf("|all threads : %d |Hello World from thread %d\n", omp_get_num_threads(), id);
     }
     // parallel region end
     return 0;
