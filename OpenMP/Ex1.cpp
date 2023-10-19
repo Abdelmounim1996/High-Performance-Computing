e214201c85a1f5dcbc267bec383f56124d0ea7d1
Abdelmounim1996 <abdelmounim.elkorchi@gmail.com>
2023-10-20 00:44:52 +0200

    Ex1 for

--- OpenMP/Ex1.cpp
+++ OpenMP/Ex1.cpp
@@ -1,10 +1,19 @@
-
 #include <cstdio>
 #include <omp.h>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
+// region parallel begin
+#pragma omp parallel num_threads(5)
+    {
+#pragma omp for
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                printf("|task %d achieved by proc %d |\n", i, omp_get_thread_num());
+            }
+        }
+    }
 
     return 0;
 }
